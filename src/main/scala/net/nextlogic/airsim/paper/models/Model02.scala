276a49944d83dcef29dcf4c0d20b22f4e8dcae59
Peter Bohm <peter@nextlogic.biz>
2019-09-12 14:47:11 +1000

    Moved models to common folder

--- src/main/scala/net/nextlogic/airsim/paper/models/Model02.scala
+++ src/main/scala/net/nextlogic/airsim/paper/models/Model02.scala
@@ -1,28 +1,26 @@
-package net.nextlogic.airsim.paper.model01
+package net.nextlogic.airsim.paper.models
 
 import java.net.InetSocketAddress
 import java.sql.Timestamp
-
-import akka.actor.{ActorSystem, Props}
 import akka.pattern.ask
+import akka.actor.{ActorSystem, Props}
 import akka.routing.{Broadcast, SmallestMailboxPool}
 import akka.stream.alpakka.slick.javadsl.SlickSession
 import akka.stream.alpakka.slick.scaladsl.Slick
 import akka.stream.scaladsl.{Sink, Source, SourceQueueWithComplete}
 import akka.stream.{ActorMaterializer, OverflowStrategy}
 import akka.util.Timeout
-import net.nextlogic.airsim.api.rpc.{AirSimDataHandler, MsgPackRpcActor}
 import net.nextlogic.airsim.api.rpc.MsgPackRpcActor.{AirSimRequest, RpcConnect}
+import net.nextlogic.airsim.api.rpc.{AirSimDataHandler, MsgPackRpcActor}
 import net.nextlogic.airsim.paper.persistence.SteeringDecision
 import net.nextlogic.airsim.paper.sensors.location.RelativePosition
 import net.nextlogic.airsim.paper.solvers.HCMertzSolver
 import net.nextlogic.airsim.paper.{AirsimUtils, Constants}
-
-import scala.concurrent.Await
 import scala.concurrent.duration._
+import scala.concurrent.Await
 
-object Model01 extends App {
-  implicit val system = ActorSystem("paper-model-01")
+object Model02 extends App {
+  implicit val system = ActorSystem("paper-model-02")
   implicit val materializer = ActorMaterializer()
   implicit val executionContext = system.dispatcher
   implicit val timeout = Timeout(1.seconds)
@@ -55,7 +53,7 @@ object Model01 extends App {
 
   Thread.sleep(Constants.initialPositionWait)
 
-  airSimPoolMaster ? AirSimRequest("simPause", Array(true))
+  // airSimPoolMaster ? AirSimRequest("simPause", Array(true))
   Thread.sleep(100)
 
   var eTheta = Math.cos(0.5)
@@ -64,31 +62,40 @@ object Model01 extends App {
   val startTime = System.currentTimeMillis()
 
   (1 to 300).foreach{i =>
-    val eLocationFuture = airSimPoolMaster ? AirSimRequest("simGetGroundTruthKinematics", Array(Constants.e))
-    val eLocation = AirsimUtils.getPositionBlocking(eLocationFuture)
-    val eLocationTime = System.currentTimeMillis()
-    val pLocationFuture = airSimPoolMaster ? AirSimRequest("simGetGroundTruthKinematics", Array(Constants.p))
-    val pLocation = AirsimUtils.getPositionBlocking(pLocationFuture)
-    val pLocationTime = System.currentTimeMillis()
-    val relPos = RelativePosition(eLocation, eTheta, pLocation, pTheta)
+    val eLocationE = AirsimUtils.getPositionBlocking(airSimPoolMaster ? AirSimRequest("simGetGroundTruthKinematics", Array(Constants.e)))
+    val eLocationTimeE = System.currentTimeMillis()
+    val pLocationE = AirsimUtils.getPositionBlocking(airSimPoolMaster ? AirSimRequest("simGetGroundTruthKinematics", Array(Constants.p)))
+    val pLocationTimeE = System.currentTimeMillis()
+    val eRelPos = RelativePosition(eLocationE, eTheta, pLocationE, pTheta)
 
-    val ePhi = HCMertzSolver.evade(relPos)
-    val pPhi = HCMertzSolver.pursue(relPos)
+    val ePhi = HCMertzSolver.evade(eRelPos)
 
-    steeringDecisions offer SteeringDecision(Constants.e, relPos.pRelativePosition, eLocation,
-      eLocationTime, pLocation, pLocationTime, eTheta, pTheta, ePhi, System.currentTimeMillis() - startTime)
-    steeringDecisions offer SteeringDecision(Constants.p, relPos.pRelativePosition, pLocation,
-      pLocationTime, eLocation, eLocationTime, pTheta, eTheta, pPhi, System.currentTimeMillis() - startTime)
+    steeringDecisions offer SteeringDecision(Constants.e, eRelPos.pRelativePosition, eLocationE,
+      eLocationTimeE, pLocationE, pLocationTimeE, eTheta, pTheta, ePhi, System.currentTimeMillis() - startTime)
 
     eTheta = ePhi
+    // moveByVelocity is a non-blocking call and returns after timeout provided
+    // but we don't need to wait for the response - any other move request will interrupt the previous one
+    airSimPoolMaster ? AirSimRequest("moveByVelocityZ", AirsimUtils.moveByVelocityZArgs(Constants.e, eTheta, Constants.eVelocity))
+
+    val eLocationP = AirsimUtils.getPositionBlocking(airSimPoolMaster ? AirSimRequest("simGetGroundTruthKinematics", Array(Constants.e)))
+    val eLocationTimeP = System.currentTimeMillis()
+    val pLocationP = AirsimUtils.getPositionBlocking(airSimPoolMaster ? AirSimRequest("simGetGroundTruthKinematics", Array(Constants.p)))
+    val pLocationTimeP = System.currentTimeMillis()
+    val pRelPos = RelativePosition(eLocationP, eTheta, pLocationP, pTheta)
+
+    val pPhi = HCMertzSolver.pursue(pRelPos)
+
+    steeringDecisions offer SteeringDecision(Constants.p, pRelPos.pRelativePosition, pLocationP,
+      pLocationTimeP, eLocationP, eLocationTimeP, pTheta, eTheta, pPhi, System.currentTimeMillis() - startTime)
+
     pTheta = pPhi
 
-    airSimPoolMaster ? AirSimRequest("moveByVelocityZ", AirsimUtils.moveByVelocityZArgs(Constants.e, eTheta, Constants.eVelocity))
+    // moveByVelocity is a non-blocking call and returns after timeout provided
+    // but we don't need to wait for the response - any other move request will interrupt the previous one
     airSimPoolMaster ? AirSimRequest("moveByVelocityZ", AirsimUtils.moveByVelocityZArgs(Constants.p, pTheta, Constants.pVelocity))
 
-    (airSimPoolMaster ? AirSimRequest("simContinueForTime", Array(Constants.stepLength / 1000.0))).map{_ =>
-      Thread.sleep(Constants.stepLength)
-    }
+    Thread.sleep(Constants.stepLength)
 
 
   }
@@ -104,7 +111,7 @@ object Model01 extends App {
     val steeringDecisions = Source.queue[SteeringDecision](100, OverflowStrategy.dropHead)
       .via(Slick.flow(4, p =>
         sqlu"""INSERT INTO steering_decisions (label, run, name, time, rel_pos_x, rel_pos_y, my_pos_x, my_pos_y, my_pos_time, opp_pos_x, opp_pos_y, opp_pos_time, my_theta, opp_theta, phi) VALUES
-                ('Model 01',
+                ('Model 02B (calculate and update together)',
                   $run, ${p.name}, ${p.time}, ${p.relativePosition.x}, ${p.relativePosition.y},
                   ${p.myPosition.x}, ${p.myPosition.y}, ${p.myPositionTime},
                   ${p.opponentPosition.x}, ${p.opponentPosition.y}, ${p.oppPositionTime},
